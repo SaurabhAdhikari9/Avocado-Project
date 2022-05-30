e3ab3a9cd48f67c2a6b08efa39b2b78a55ba7112
Saurabh Adhikari <83704793+SaurabhAdhikari9@users.noreply.github.com>
2022-05-30 14:04:17 +0545

    form fields were created

--- avocado-project/src/App.js
+++ avocado-project/src/App.js
@@ -1,4 +1,31 @@
 import './App.css';
+import React  from 'react'
+import { UserOutlined, capacityField, LockOutlined, NumberOutlined,UploadOutlined } from '@ant-design/icons';
+import { Input, Button, message, Upload, Checkbox } from 'antd';
+const props = {
+  name: 'file',
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  headers: {
+    authorization: 'authorization-text',
+  },
+
+  onChange(info) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+
+const onChange = (e) => {
+  console.log(`checked = ${e.target.checked}`);
+};
+
 function App() {
   return (
 <div className='main'>
@@ -8,9 +35,58 @@ function App() {
         Create Table:
       </titleLabel>
     </formTitle>
-  </div>
+    <hr/>
+    <formContent>
+      
+      <layout>
+        <layoutLabel>Layout:</layoutLabel>
+        <layoutField>
+        <Input size="large" placeholder="  Select Layout"  />
+        </layoutField>
+      </layout>
+      
+      <name>
+        <nameLabel>
+          Name:
+        </nameLabel>
+        <nameField>
+        <Input size="large" placeholder="  Enter Name" />
+        </nameField>
+      </name>
+      
+      <capacity>
+        <capacityLabel>
+          Capacity:
+        </capacityLabel>
+        <capacityField>
+        <Input size="large" placeholder="  Enter number of capacity"  />
+        </capacityField>
+      </capacity>
 
+      <status>
+        <statusLabel>
+          Status:
+        </statusLabel>
+        <statusField>
+        <Checkbox onChange={onChange}/>
+        </statusField>
+      </status>
+
+      <image>
+        <imageLabel>
+          Image:
+        </imageLabel>
+        <imageField>
+        <Upload {...props}>
+         <Button icon={<UploadOutlined />}>Click to Upload</Button>
+         </Upload>
+        </imageField>
+      </image>
+    </formContent>
+  </div>
 </div>
   );
 }
 export default App;
+
+
